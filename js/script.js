10a93d6a25ceefa1915112ef0169d2099fc7ee9d
Firman-28 <firmansukadamaigulo@gmail.com>
2026-03-11 17:03:10 +0700

    pesan update

--- js/script.js
+++ js/script.js
@@ -64,7 +64,7 @@ document.addEventListener('DOMContentLoaded', function() {
             let email = form.querySelector('input[name="email"]').value;
             let pesan = form.querySelector('textarea[name="pesan"]').value;
 
-            let nomor = "6285715169864"; // ganti dengan nomor WhatsApp kamu
+            let nomor = "6281514397312"; // ganti dengan nomor WhatsApp kamu
 
             let url = "https://wa.me/" + nomor +
             "?text=Halo saya dari website anda%0A%0A" +
